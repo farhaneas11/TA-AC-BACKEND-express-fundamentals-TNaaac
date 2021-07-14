941a657939dfadb9efb3bf510f3d23c01fd3a233
farhaneas11 <farhan.salam11@gmail.com>
2021-07-14 14:38:25 +0530

    express-fund-31

---
+++ block-BNaabm/sample/index.js
@@ -0,0 +1,18 @@
+var express = require('express')
+
+var app = express();
+
+function logger(req,res,next){
+    console.log(req.method , req.url);
+    next();
+}
+
+app.use(logger);
+
+app.get('/' , (req ,res)=>{
+    res.send('welcome');
+})
+
+app.listen(4000 , ()=>{
+    console.log('server open')
+})
